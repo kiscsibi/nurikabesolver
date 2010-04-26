76e2df30b64b5bf5b3fca8dbf7f4ff6fcb380a16
xtroce <xtroce@localhost>
2010-04-26 13:17:34 +0200

    minor fixes 2

--- src/Mainwindow.java
+++ src/Mainwindow.java
@@ -89,7 +89,7 @@ public class Mainwindow extends javax.swing.JFrame {
 			    graphics.fillRect(w*width, h*height, width, height);
 			    graphics.setColor(Color.black);
 			    int lim = Engine.getLimit(w, h);
-			    if( lim != null ) {
+			    if( lim != -1 ) {
 				graphics.drawString(Integer.toString(Engine.getLimit(w,h)), (int)(w*width+0.25*width), (int)(h*height+0.25*height));
 			    }
 			}	
