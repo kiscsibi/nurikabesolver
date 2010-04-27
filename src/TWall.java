5cdef7c85d3c220e02d1dca3c97ffaf50f89cbaa
xtroce <xtroce@localhost>
2010-04-27 16:03:12 +0200

    lots of doc, heuristics, fixes

--- src/TWall.java
+++ src/TWall.java
@@ -18,7 +18,7 @@ public class TWall extends TStructure {
 	    super(limit);
 	    addCell(cell);
 	    Limit = limit;
-    }
+	 }
 
 	public boolean isFull() {
 	    if(Limit == Cells.size()) {
@@ -27,22 +27,49 @@ public class TWall extends TStructure {
 	    return false;
 	}
 	
+	/**
+	 * checks if a wall can reach a cell
+	 * @param goal The cell to be reached
+	 * @return boolean
+	 */
 	public boolean isReachable(TCell goal) {
-		return isReachable(goal, Cells, stillToPlace());
+	    Set<TCell> hist = new HashSet<TCell>();
+	    return getReachables(Cells, hist, Limit-Cells.size()).contains(goal);
 	}
-	
-	public boolean isReachable(TCell goal, Collection<TCell> Col, int togo) {
-		Set<TCell> pos = new HashSet<TCell>();
-		
-		if(Col.contains(goal.Position))
-			return true;
-		if(togo == 0 )
-			return false;
-		
-		for(TCell c : Col) {
-				pos.addAll( (Collection<TCell>) c.getNBGreys());
+
+	/**
+	 * returns the reachable grey cells of the wall
+	 * @param ext the cells to extend
+	 * @param hist the already visited cells
+	 * @param togo the amount of steps left
+	 * @return all grey cells that are reachable
+	 */
+	private Set<TCell> getReachables(Collection<TCell> ext, Set<TCell> hist, int togo) {
+	    
+	    Set<TCell> next = new HashSet<TCell>();
+	    
+	    if(togo == 0) {
+		return hist;
+	    }
+	    else {
+		for(TCell c : ext) {
+		    for(TCell g : c.getNBGrays()) {
+			if(!ext.contains(g)) {
+			    ext.addAll( (Collection<TCell>) c.getNBGrays());
+			}
+		    }
 		}
-		
-		return isReachable(goal, pos, togo-1);	
+	    }
+	    hist.addAll(ext);	    
+	    return getReachables(next, hist, togo-1);
+	}
+
+	/**
+	 * wrapper for getReachables(ext, hist, togo)
+	 * @return all grey cells that are reachable
+	 */
+	public Set<TCell> getReachables() {
+	    Set<TCell> hist = new HashSet<TCell>();
+	    return getReachables(Cells, hist, Limit-Cells.size());
 	}
 } 
