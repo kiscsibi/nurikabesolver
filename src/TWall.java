fb0a84a32a337b65dca5ccc8f54829495ed52d25
Sebastian Droeppelmann <xtroce@gmail.com>
2010-05-22 15:04:08 +0200

    merge

--- src/TWall.java
+++ src/TWall.java
@@ -1,10 +1,14 @@
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
 
 public class TWall extends TStructure {
 
+	Queue<TCell> Reachables;
+	
     /******************************
      * OVERRIDES
      ******************************/
@@ -34,6 +38,8 @@ public class TWall extends TStructure {
 	super(limit);
 	addCell(cell);
 	Limit = limit;
+	Reachables = new LinkedList<TCell>();
+	
     }
 
     /**
